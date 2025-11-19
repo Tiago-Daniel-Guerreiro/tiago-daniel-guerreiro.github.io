2d21ee906cd35ee8c3fbd8d8e811c34522e460c3
Tiago Guerreiro <tiagodguerreiro@gmail.com>
2025-11-19 10:57:46 +0000

    Atualização dos dados dos projetos Reordenação dos projetos Melhoraria dos estilos de layout e transições no CSS

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ const App = {
                 "demoLink": null,
                 "tags": ["C#", "Windows Forms", "JSON"]
             },
-            {
+                        {
                 "id": 3,
                 "repo": "SistemaHospitalar",
                 "coverImage": null,
@@ -231,15 +231,6 @@ const App = {
             },
             {
                 "id": 4,
-                "repo": "tiago-daniel-guerreiro.github.io",
-                "coverImage": null,
-                "images": null,
-                "demoLink": "https://tiago-daniel-guerreiro.github.io",
-                "tags": ["CSS", "JavaScript","HTML"],
-                "showInCards": false
-            },
-            {
-                "id": 5,
                 "repo": "Site-Manga",
                 "coverImage": null,
                 "images": null,
@@ -247,7 +238,7 @@ const App = {
                 "tags": ["JavaScript","CSS","HTML"]
             },
             {
-                "id": 6,
+                "id": 5,
                 "repo": "Site-Agendamentos",
                 "coverImage": "",
                 "images": null,
@@ -255,12 +246,21 @@ const App = {
                 "tags": ["PHP", "MySQL"]
             },
             {
-                "id": 7,
+                "id": 6,
                 "repo": "SistemaDeGestaoDeBiblioteca",
                 "coverImage": null,
                 "images": null,
                 "demoLink": null,
                 "tags": ["Python", "JSON"]
+            },
+            {
+                "id": 7,
+                "repo": "tiago-daniel-guerreiro.github.io",
+                "coverImage": null,
+                "images": null,
+                "demoLink": "https://tiago-daniel-guerreiro.github.io",
+                "tags": ["CSS", "JavaScript","HTML"],
+                "showInCards": false
             }
         ],
         
@@ -1095,8 +1095,7 @@ const App = {
                     this.contador = 0;
                     // procura o projeto do portfolio por githubLink OU demoLink e abre o modal
                     const projeto = App.projetos.dadosProjetos.find(p => 
-                        (p.githubLink && p.githubLink.includes('tiago-daniel-guerreiro.github.io')) ||
-                        (p.demoLink && p.demoLink.includes('tiago-daniel-guerreiro.github.io'))
+                        p.repo === 'tiago-daniel-guerreiro.github.io'
                     );
                     if (projeto)
                         App.projetos.abrirModal(projeto.id);
